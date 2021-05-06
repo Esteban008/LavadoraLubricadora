e3bf87af1d896b9994dee94cd7131bdc05efe787
ESTEBAN LEANDRO MACHADO OBANDO <esteban.machado@epn.edu.ec>
2021-05-06 14:17:09 -0500

    Interfaz grafica frmPrincipal

--- LavadoraLubricadora/Principal.cs
+++ LavadoraLubricadora/Principal.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,5 +17,34 @@ namespace LavadoraLubricadora
         {
             InitializeComponent();
         }
+
+        private void Principal_Load(object sender, EventArgs e)
+        {
+
+        }
+        #region Panel de Control
+        //Boton que permite cerrar el programa
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        //Boton que permite minimizar el programa
+        private void btnMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+        /// Metodo que nos permite arrastrar
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        //Evento sobre el panel a arrastrar
+        private void panelBarraTitulo_MouseMove_1(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+        #endregion
     }
 }
